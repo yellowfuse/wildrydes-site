206c65f143fe4919d61dbf9b9d4ac857288c9961
Steven Fokkinga <stevenfokkinga@Stevens-MacBook-Pro.local>
2021-06-16 18:22:54 +0200

    Added invokeUrl

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://jg7s01dlr9.execute-api.eu-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
