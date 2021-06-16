77a85328a1e7d8ea731ca266124bc7f156f7699d
Steven Fokkinga <stevenfokkinga@Stevens-MacBook-Pro.local>
2021-06-16 17:49:05 +0200

    Updated config.js with Cognito details

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_Aq9ExMBwu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4dkrvdbgc58prqcv7c3l2r5974', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
